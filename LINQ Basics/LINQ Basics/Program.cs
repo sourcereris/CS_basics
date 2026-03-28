ef6fb1e9b76a92ea7fbb5b764abd503af5778560
sourcereris <rimedimas@gmail.com>
2026-03-28 13:08:35 +0900

    Update Program.cs

--- LINQ Basics/LINQ Basics/Program.cs
+++ LINQ Basics/LINQ Basics/Program.cs
@@ -1,33 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿int[] ints = {1, 2, 4 ,5 ,4 , 6};
 
-public class Unit
-{
-    public string Name { get; set; }
-    public string Role { get; set; } // "Tank", "DPS", "Healer"
-    public int Health { get; set; }
-    public int MaxHealth { get; set; }
-    public int Initiative { get; set; }
-}
+Find(ints);
 
-public class Program
+void Find(int[] a) 
 {
-    public static void Main()
+    Dictionary<int, int> last = new Dictionary<int, int>();
+    last.Add(0, a[0]);
+
+    for (int i = 1; i < a.Length; i++) 
     {
-        List<Unit> party = new List<Unit>
-        {
-            new Unit { Name = "Alistair", Role = "Tank", Health = 120, MaxHealth = 120, Initiative = 15 },
-            new Unit { Name = "Morrigan", Role = "DPS", Health = 0, MaxHealth = 80, Initiative = 45 },
-            new Unit { Name = "Leliana", Role = "DPS", Health = 40, MaxHealth = 75, Initiative = 60 },
-            new Unit { Name = "Wynne", Role = "Healer", Health = 60, MaxHealth = 60, Initiative = 30 }
-        };
-
-        var aliveUnits = party.Where(u => u.Health > 0);
-        var sortedByInitiative = aliveUnits.OrderByDescending(u => u.Initiative);
-        bool healthyUnit = sortedByInitiative.Any(u => u.Health < u.MaxHealth / 2.0f);
-        var healer = aliveUnits.FirstOrDefault(u => u.Role == "Healer");
-
-        List<string> nameList = party.Select(u => u.Name).ToList();
+        if (last.ContainsKey(a[i])) { last[a[i]]++; }
+        else { last.Add(i, a[i]); }
     }
+
+    int aa = last.Keys.Max();
+
+    Console.WriteLine(last[aa]);
 }
