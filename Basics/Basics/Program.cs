9e2821f8c0acee62d035f05a9e6276c871e4c289
sourcereris <rimedimas@gmail.com>
2026-06-18 01:34:36 +0900

    Clean up Basics project and set up Task 2 data/skeleton

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -1,109 +1,71 @@
-﻿using Basics;
 using System;
 using System.Collections.Generic;
 
-//Player p1 = new Player("Alice", 100, 20);
-//p1.TakeDamage(DamageCalculator.CalculateDamage(50, p1.Armour));
-
-Dictionary<string, int> inventory = new();
-
-AddItem("Sword", 1);
-AddItem("Shield", 1);
-AddItem("Coin", 100);
-AddItem("Coin", 50);
-
-PrintInventory();
-
-
-// To simplyfy we will pretend that we have a data base of items in a txt file or smth
-// and we will use that txt file as a reference to fill our InvenotryDatabase.itemWeights Dictioary.
-// also we will use the same txt for filling our player inventory.
-InventoryManager playerInventory = new InventoryManager();
-
-playerInventory.AddItem("Potion", 5);
-playerInventory.AddItem("Rock", 3);
-
-float InventoryWeight()
+Dictionary<string, string> dictionary = new()
 {
-    float sum = 0;
-    foreach(var item in playerInventory.Inventory)
-    {
-        ItemDatabase.ItemWeights.TryGetValue(item.Key, out float weight);
-        sum += weight * item.Value;
-    }
+    { "Hello", "Labas" },
+    { "Goodbye", "Viso gero" },
+    { "Thank you", "Ačiū" },
+    { "Please", "Prašau" },
+    { "Yes", "Taip" },
+    { "No", "Ne" },
+    { "Friend", "Draugas" },
+    { "Bread", "Duona" },
+    { "Water", "Vanduo" },
+    { "Sun", "Saulė" }
+};
 
-    return sum;
+Console.WriteLine(GetTranslation("Hello"));
+Console.WriteLine(GetTranslation("Goodbye"));
+Console.WriteLine(GetTranslation("Thanks"));
+
+string GetTranslation(string englishKey) 
+{
+    if (dictionary.TryGetValue(englishKey, out string? lithuanianTranslation))
+    {
+        return lithuanianTranslation;
+    }
+    
+    return $"[{englishKey}]";
 }
 
-void AddItem(string itemName, int amount) 
+// ==========================================
+// Task 2: The Crafting Validator
+// ==========================================
+
+Dictionary<string, int> recipe = new Dictionary<string, int>()
 {
-    if (inventory.TryGetValue(itemName, out int currentAmount))
-    {
-        inventory[itemName] = currentAmount + amount;
-    }
-    else
-    {
-        inventory.Add(itemName, amount);
-    }
-}
+    { "Iron Ingot", 2 },
+    { "Wood", 1 },
+    { "Leather Strap", 1 }
+};
 
-void PrintInventory() 
+Dictionary<string, int> playerInventory = new Dictionary<string, int>()
 {
-    foreach (var item in inventory)
-    {
-        Console.WriteLine($"{item.Key}: {item.Value}");
-    }
-}
+    { "Iron Ingot", 5 },
+    { "Wood", 10 }
+    // Leather Strap is missing completely!
+};
 
-class Player 
+bool canCraftSword = CanCraft(playerInventory, recipe);
+Console.WriteLine($"\nCan craft Steel Sword? {canCraftSword}");
+
+bool CanCraft(Dictionary<string, int> inventory, Dictionary<string, int> recipeCost)
 {
-    private string _name;
-    private int _hp;
-    private int _armour;
-
-    public Player(string name, int hp, int armour) 
+    foreach (var item in recipeCost) 
     {
-        _name = name;
-        _hp = hp;
-        _armour = armour;
-    }
-
-    // all methods below do the same thing, just different syntax for properties, right?
-
-    //public int Armour() { return _armour; }
-    //public int Armour => _armour;
-    public int Armour {get { return _armour; } private set { } }
-    public int HP { get => _hp; private set { } }
-    public string Name { get => _name; private set { } }
-
-    public void TakeDamage(int damage)
-    {
-        if (_hp > damage)
+        if (inventory.TryGetValue(item.Key, out int available)) 
         {
-            _hp -= damage;
-            Console.WriteLine($"{_name} took {damage} damage and has {_hp} HP left.");
+            if (item.Value > available) 
+            {
+                return false;
+            }
         }
         else
         {
-            Console.WriteLine($"{_name} died");
-            _hp = 0;
+            return false;
         }
     }
-    public void SetArmour(int armour)
-    {
-        _armour = armour;
-        Console.WriteLine($"{_name} now has {_armour} armour.");
-    }
-}
-
-class DamageCalculator
-{
-    //staticm ethod used as utility for calculating damage after considering armour,
-    //not only i player class but also other classes can use this method
-    //without needing to create an instance of DamageCalculator
-    public static int CalculateDamage(int baseDamage, int armour)
-    {
-        int damageAfterArmour = baseDamage - armour;
-        return Math.Max(0, damageAfterArmour);
-    }
+    
+    return true;
 }
