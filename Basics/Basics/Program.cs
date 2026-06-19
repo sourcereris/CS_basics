42d1ca38aff953011c58ec62b2c908ecf9499894
sourcereris <rimedimas@gmail.com>
2026-06-19 22:42:09 +0900

    Implement basic Enemy and Goblin polymorphism in Program.cs

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -11,3 +11,33 @@ while(UIScreens.Count > 0)
 {
     Console.WriteLine($"Closing Screen: {UIScreens.Pop()}");
 }
+
+List<Enemy> enemies = new List<Enemy>
+{
+    new Enemy(),
+    new Goblin(),
+    new Enemy(),
+    new Goblin(),
+    new Goblin()
+};
+
+foreach (var enemy in enemies) 
+{
+    enemy.Attack();
+}
+
+public class Enemy 
+{
+    public virtual void Attack()
+    {
+        Console.WriteLine("The enemy attacks!");
+    }
+}
+
+public class Goblin : Enemy
+{
+    public override void Attack()
+    {
+        Console.WriteLine("The goblin swings a club!");
+    }
+}
